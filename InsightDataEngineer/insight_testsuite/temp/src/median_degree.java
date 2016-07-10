fd02f270a9d3e55e4ee55c67b385760b0f728125
keyuls <keyul.shah1@gmail.com>
2016-07-09 18:18:39 -0700

    Add comments

--- InsightDataEngineer/insight_testsuite/temp/src/median_degree.java
+++ InsightDataEngineer/insight_testsuite/temp/src/median_degree.java
@@ -1,103 +1,96 @@
 import java.util.*;
 import java.io.*;
 import java.math.BigDecimal;
-import java.security.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import org.json.JSONObject;
-import org.json.JSONArray;
 import org.json.JSONException;
 
 public class median_degree {
 
-	static Date currentTime =null;
-	static Date max_span=null;
-	static Map<edge,Date> liveEdges= new HashMap<edge,Date>();
-	static Map<String,Integer> userConnect= new HashMap<String,Integer>();
-	static BufferedWriter bw; 
+	private Date currentTime =null;
+	private Date max_span=null;
+	private Map<edge,Date> liveEdges= new HashMap<edge,Date>();
+	private Map<String,Integer> userConnect= new HashMap<String,Integer>();
+	private BufferedWriter bw; 
+	private BufferedReader br;
 	
-	public static void main(String[] args) throws IOException, JSONException {
-
+	public static void main(String[] args) throws IOException, JSONException, ParseException {
+		median_degree md = new median_degree();
+		md.collectData();
+	}
+	
+	/*Fetch data from input file*/
+	public void collectData() throws IOException, JSONException, ParseException{
+		String line;	
 		Reader reader = new FileReader("./venmo_input/venmo-trans.txt");
 		Writer writer = new FileWriter("./venmo_output/output.txt");
-
-		BufferedReader br = new BufferedReader(reader);
+		br = new BufferedReader(reader);
 		bw = new BufferedWriter(writer);
 		
-		String line;
 		while((line=br.readLine())!=null){
 			JSONObject jobj = new JSONObject(line);
 			processData(jobj);		
 		}
 		bw.close();
-
 	}
 	
-	public static void processData(JSONObject jobj) throws JSONException{
-		String actor= jobj.getString("actor");
-		String target=jobj.getString("target");
-		String createdAt = jobj.getString("created_time");
+	/*Processing each of Json Object of Transaction*/
+	public void processData(JSONObject jobj) throws JSONException,IOException,ParseException{
+		if(jobj.getString("actor")!=null && jobj.getString("target")!=null){
+			String actor= jobj.getString("actor");
+			String target=jobj.getString("target");
+			String createdAt = jobj.getString("created_time");
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-		sdf.setLenient(true);
-		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+			sdf.setLenient(true);
 			currentTime= sdf.parse(createdAt);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
 			
-		if(max_span==null || currentTime.compareTo(max_span)>0){
-			max_span=currentTime;
-			edge e=new edge(actor, target);
-			liveEdges.put(e, currentTime);
-			updateCount(actor);
-			updateCount(target);
-			removeOld(max_span);
-			
-		}
-		else if((currentTime.compareTo(max_span)<=0)  && ((max_span.getTime() - currentTime.getTime()) / 1000) < 60){	
-			edge e=new edge(actor, target);
-			liveEdges.put(e, currentTime);
-			updateCount(actor);
-			updateCount(target);
-			
-		}
-		try {
-			getMedian();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			if(max_span==null || currentTime.compareTo(max_span)>0){
+				max_span=currentTime;
+				edge e=new edge(actor, target);
+				liveEdges.put(e, currentTime);
+				updateCount(actor);
+				updateCount(target);
+				removeOld(max_span);
+
+			}
+			else if((currentTime.compareTo(max_span)<=0)  && ((max_span.getTime() - currentTime.getTime()) / 1000) < 60){	
+				edge e=new edge(actor, target);
+				liveEdges.put(e, currentTime);
+				updateCount(actor);
+				updateCount(target);
+
+			}
+			getMedian();	
+		}		
 	}
 	
-	public static void updateCount(String userName) {
-
-		if(userConnect.containsKey(userName)){
+	/*Increment total connected edges with specific user*/
+	public void updateCount(String userName) {
+		if(userConnect.containsKey(userName))
 			userConnect.put(userName, userConnect.get(userName)+1);
-		}
 		else
 			userConnect.put(userName, 1);
 	}
-	
-	public static void deleteCount(String userName) {
 
+	/*Decrement total connected edges with specific user*/
+	public void deleteCount(String userName) {
 		if(userConnect.containsKey(userName)){
-			userConnect.put(userName, userConnect.get(userName)-1);
-			
+			userConnect.put(userName, userConnect.get(userName)-1);			
 			if(userConnect.get(userName)<1)
 				userConnect.remove(userName);
 		}
-		
 	}	
 	
-	public static void removeOld( Date max_span){
-		
+	/*remove edges that are older then 60 seconds*/
+	@SuppressWarnings("unchecked")
+	public void removeOld( Date max_span){	
 		Iterator itr = liveEdges.entrySet().iterator();
 		while(itr.hasNext()){
-			Map.Entry pair = (Map.Entry)itr.next();
+			Map.Entry<edge,Date> pair = (Map.Entry<edge,Date>)itr.next();
 			Date userTime = (Date)pair.getValue();
 			if(((max_span.getTime()-userTime.getTime())/1000)>60){
-				//System.out.println("inside--"+pair.getValue());
 				edge e= (edge)pair.getKey();
 				deleteCount(e.getActor());
 				deleteCount(e.getTarget());
@@ -105,25 +98,26 @@ public class median_degree {
 			}
 		}	
 	}
-	
-	public static void getMedian() throws IOException{
+
+	/*calculating median*/
+	public void getMedian() throws IOException{
+		double med_val=0.0;
+		int middle =0;
 		List<Integer> median = new ArrayList<Integer>();
 		for(Map.Entry<String, Integer> entry : userConnect.entrySet()){
 			median.add(entry.getValue());
-			//System.out.println(entry.getKey()+"--"+entry.getValue());
-		}
-		
+		}		
 		Collections.sort(median);
-		int middle = median.size()/2;
-		double med_val=0.0;
+		middle = median.size()/2;
+		
 		if((middle%2)==1)
 			med_val=(double)median.get(middle);
 		else
 			med_val=(double)(median.get(middle-1)+median.get(middle))/2;
-
 		
-		System.out.println(	new BigDecimal(med_val).setScale(2,BigDecimal.ROUND_FLOOR));	
 		bw.write((new BigDecimal(med_val).setScale(2,BigDecimal.ROUND_FLOOR)).toString() + "\n");
+		
+		//System.out.println(	new BigDecimal(med_val).setScale(2,BigDecimal.ROUND_FLOOR));	
 		//System.out.println("---");
 	}
 
